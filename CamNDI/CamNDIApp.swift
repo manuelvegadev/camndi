c41786e30c06e954a7fb4b6cc7ad57663a19b982
Manuel Vega <jumavela2013@gmail.com>
2026-03-16 14:16:32 -0500

    Initial Commit

---
+++ CamNDI/CamNDIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamNDIApp.swift
+//  CamNDI
+//
+//  Created by Manuel Vega on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CamNDIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
